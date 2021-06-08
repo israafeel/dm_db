836f919788ff9aaed06258e79d21fd09838c9758
Israafeel <niqiesehe@qq.com>
2021-06-08 21:41:16 +0800

    # commit

--- src/dm_main.cpp
+++ src/dm_main.cpp
@@ -1,17 +1,15 @@
 // std
-#include "stdio.h"
-#include <string.h>
-#include <stdbool.h>
+#include <iostream>
 // deps
-#include "db_type.h"
+#include "db_type.hpp"
 #include "get_opt.hpp"
 
-int main(){
-    bool help = get_arg( false, "-h", "--help", "-?" );
-    int version = get_arg( 0, "-v", "--version", "--show-version" );
-    int depth = get_arg( 1, "-d", "--depth", "--max-depth");
-
-    std::cout << help << ',' << version << ',' << depth << std::endl;
-
-    return RET_CODE_SUCCESS;
-}
+//int main(){
+//    bool help = get_arg( false, "-h", "--help", "-?" );
+//    int version = get_arg( 0, "-v", "--version", "--show-version" );
+//    int depth = get_arg( 1, "-d", "--depth", "--max-depth");
+//
+//    std::cout << help << ',' << version << ',' << depth << std::endl;
+//
+//    return RET_CODE_SUCCESS;
+//}
